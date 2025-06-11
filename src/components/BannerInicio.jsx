32bc55af648c87332b79e6bfda0c66281bb22c5d
diego <diego.chervin@hotmail.com>
2025-06-11 20:52:33 -0300

    foto cel

--- src/components/BannerInicio.jsx
+++ src/components/BannerInicio.jsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function BannerInicio() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Limpieza al desmontar el componente
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const imageSrc = isMobile
+    ? "/img/banner_home_mobile.png"
+    : "/img/afnanfebrero.jpg";
+
   return (
     <div
       style={{
         width: "100vw",
-        height: "53vh", 
+        height: "53vh",
         overflow: "hidden",
       }}
     >
       <Link to="/perfumes">
         <img
-          src="/img/afnanfebrero.jpg"
+          src={imageSrc}
           alt="Banner"
           style={{
             width: "100%",
