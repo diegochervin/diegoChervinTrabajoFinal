131071f5fd47abd77d1a9a466e4c147003d595c4
diego <diego.chervin@hotmail.com>
2025-06-11 20:38:58 -0300

    Update BannerInicio.jsx

--- src/components/BannerInicio.jsx
+++ src/components/BannerInicio.jsx
@@ -1,28 +1,11 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 function BannerInicio() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    handleResize(); // Ejecutar al inicio
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <div
       style={{
         width: "100vw",
-        height: isMobile ? "60vh" : "53vh",
-        backgroundColor: "#000",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
+        height: "53vh", 
         overflow: "hidden",
       }}
     >
@@ -33,7 +16,7 @@ function BannerInicio() {
           style={{
             width: "100%",
             height: "100%",
-            objectFit: isMobile ? "cover" : "contain",
+            objectFit: "cover", 
             display: "block",
             cursor: "pointer",
           }}
